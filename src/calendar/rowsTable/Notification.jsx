db5320f21fe1d912d3ecb4ab139a8730500a74b9
fercho1999leon <fercho1999_w@hotmail.com>
2022-03-01 13:20:54 -0500

    correcion en longitu de msg

--- src/calendar/rowsTable/Notification.jsx
+++ src/calendar/rowsTable/Notification.jsx
@@ -6,17 +6,17 @@ import Grid from '@mui/material/Grid';
 
 }*/
 
-export default function Notification(porps){
+export default function Notification(props){
     return (
         <>
             {
-                porps.notifications.length>0?
+                props.notifications.length>0?
                     <>
-                        <Grid item xs={12} sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center',minHeight:'30px',cursor: 'pointer', width:'100px',borderRadius:'10px', backgroundColor:'#6C64A0', transition: 'background-color 0.5s ease-out 0s', color:'white', '&:hover':{backgroundColor:'#5FBFAF'}}}>
-                            <h4 >{porps.notifications.length>0?porps.notifications[0].msg:null}</h4>
+                        <Grid item xs={12} sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center',minHeight:'30px', maxHeight:'80px',cursor: 'pointer', width:'100px',borderRadius:'10px', backgroundColor:'#6C64A0', transition: 'background-color 0.5s ease-out 0s', color:'white', '&:hover':{backgroundColor:'#5FBFAF'}}}>
+                            <h4 >{props.notifications.length>0?props.notifications[0].msg.length>50?props.notifications[0].msg.substr(0,50)+'...':props.notifications[0].msg:null}</h4>
                         </Grid>
                         <Grid item xs={12}>
-                            <h3>{porps.notifications?porps.notifications.length>1?'+'+(porps.notifications.length-1):null:null}</h3>
+                            <h3>{props.notifications?props.notifications.length>1?'+'+(props.notifications.length-1):null:null}</h3>
                         </Grid>
                     </>
                 :
